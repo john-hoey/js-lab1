c2f9aad9005a9c7a71902ff25f0ffe535be7eed0
john-hoey <john.t.hoey@gmail.com>
2021-01-26 09:07:14 -0500

    extra credit

--- script.js
+++ script.js
@@ -48,3 +48,45 @@ while (true) {
     break;
   }
 }
+
+let hours = 60;
+let wage = 10;
+let averageTakeHomeAfterTaxesUS = 0.76;
+let totalPreTaxPaycheck = wage * hours;
+
+let totalPostTaxPaycheck = totalPreTaxPaycheck * averageTakeHomeAfterTaxesUS;
+let overtimeHours = hours - 40;
+let overtimeWage = wage * 1.5;
+
+if (hours <= 40) {
+  console.log(`${wage} wage`);
+  console.log(`${hours} hours`);
+  console.log(`${totalPreTaxPaycheck} total pretax paycheck`);
+  console.log(`${totalPostTaxPaycheck} total posttax paycheck`);
+  console.log(
+    `Working ${hours} hours at a wage of ${wage} dollars an hour earned you a total of ${totalPreTaxPaycheck} before taxes. With an average US tax rate of 24% for a single payer household, your estimated take home after taxes is ${totalPostTaxPaycheck}.`
+  );
+} else {
+  console.log(`${wage} wage`);
+  console.log(`${hours} hours`);
+  console.log(`${overtimeWage} overtime wage`);
+  console.log(`${overtimeHours} overtime hours`);
+  let totalPreTaxPaycheck =
+    (hours - overtimeHours) * wage + overtimeHours * overtimeWage;
+  let totalPostTaxPaycheck = totalPreTaxPaycheck * 0.76;
+  console.log(`${totalPreTaxPaycheck} total pretax paycheck`);
+  console.log(`${totalPostTaxPaycheck} total posttax paycheck`);
+  console.log(
+    `${averageTakeHomeAfterTaxesUS} average take home % of paycheck in the United states`
+  );
+  console.log(
+    `Working ${hours} hours at a wage of ${wage} dollars an hour for the first 40 hours and ${overtimeWage} dollars an hour for the ${overtimeHours} hours of overtime you worked earned you a total of ${totalPreTaxPaycheck} before taxes. With an average US tax rate of 24% for a single payer household, your estimated take home after taxes is ${totalPostTaxPaycheck}.`
+  );
+}
+
+let weeksTillMillionare = Math.round(
+  1000000 / (totalPostTaxPaycheck * averageTakeHomeAfterTaxesUS)
+);
+console.log(
+  `If you made this same amount every week you would be a millionare in ${weeksTillMillionare} weeks.`
+);
